6835dd7d18002eec8c6343ccfd50b8504c4da716
altosaxis <61560118+altosaxis@users.noreply.github.com>
2022-07-21 13:55:13 +0530

    Created using Colaboratory

---
+++ root_lin.eqn_pair.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled1.ipynb",
+      "provenance": [],
+      "authorship_tag": "ABX9TyNZGMjJffg7Vih4+11JsPnO",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/altosaxis/AI/blob/main/root_lin.eqn_pair.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "iIrPlNUl2LKa"
+      },
+      "outputs": [],
+      "source": [
+        ""
+      ]
+    }
+  ]
+}
